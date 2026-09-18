f2928a2bde91c0796a3ca75722f9d0fd56f7d7f1
manjeets26884-creator <manjeets26884@gmail.com>
2026-09-18 14:38:22 +0530

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,22 @@
+name = "manjeet"
+print("name")
+age = 39
+print(age)
+name = "manjeet"
+print(name)
+name = input("what your name")
+print(name)
+name = "manjeet"
+print(type(name))
+marks = 75
+if marks >= 80:
+ elif marks >= 60:
+ print("good")
+ else: 
+ mtr = 670
+ if mtr > 600:
+     print("Stock is above 600")
+ else:
+     print("Stock is 600 or below")
+     "r"
+     
